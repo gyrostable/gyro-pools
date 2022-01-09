51f3288e60d095972be5e4eacc71dde4c123ff5a
Lewis Gudgeon <ljfgudgeon@gmail.com>
2022-01-09 13:49:12 +0000

    Liquidity update test

--- tests/cpmmv-2/test_math_implementations_match.py
+++ tests/cpmmv-2/test_math_implementations_match.py
@@ -163,23 +163,34 @@ def test_calculate_sqrt_price(gyro_two_math_testing, invariant, virtual_x):
         sqrt_price).approxed()
 
 
-# @given(
-#     balances=st.tuples(billion_balance_strategy, billion_balance_strategy),
-#     last_invariant=st.decimals(
-#         min_value="100", max_value="100000000", places=4),
-#     sqrt_alpha=st.decimals(min_value="0.9", max_value="0.9999", places=4),
-#     sqrt_beta=st.decimals(min_value="0.02", max_value="1.8", places=4),
-#     diff_y=st.decimals(min_value="100", max_value="1000000000", places=4))
-# def test_liquidity_invariant_update(gyro_two_math_testing, balances, sqrt_alpha, sqrt_beta, last_invariant, diff_y):
+@given(
+    balances=st.tuples(billion_balance_strategy, billion_balance_strategy),
+    sqrt_alpha=st.decimals(min_value="0.9", max_value="0.9999", places=4),
+    sqrt_beta=st.decimals(min_value="0.02", max_value="1.8", places=4),
+    diff_y=st.decimals(min_value="100", max_value="1000000000", places=4))
+def test_liquidity_invariant_update(gyro_two_math_testing, balances, sqrt_alpha, sqrt_beta, diff_y):
 
-#     if any(b < 0 for b in [balances[0], balances[1]]):
-#         return
+    if any(b == 0 for b in [balances[0], balances[1]]):
+        return
 
-#     new_invariant = math_implementation.liquidityInvariantUpdate(to_decimal(balances), sqrt_alpha,
-#                                                                  sqrt_beta, last_invariant, diff_y, True)
+    (a, mb, mc) = math_implementation.calculateQuadraticTerms(
+        to_decimal(balances), to_decimal(sqrt_alpha), to_decimal(sqrt_beta)
+    )
 
-#     new_invariant_sol = gyro_two_math_testing.liquidityInvariantUpdate(scale(balances), scale(
-#         sqrt_alpha), scale(sqrt_beta), scale(last_invariant), scale(diff_y), True)
+    if any(v < 0 for v in [a, mb, mc]):
+        return
 
-#     assert to_decimal(new_invariant_sol) == scale(
-#         new_invariant).approxed()
+    last_invariant = math_implementation.calculateInvariant(
+        to_decimal(balances), to_decimal(sqrt_alpha), to_decimal(sqrt_beta)
+    )
+
+    new_invariant = math_implementation.liquidityInvariantUpdate(to_decimal(balances), sqrt_alpha,
+                                                                 sqrt_beta, last_invariant, diff_y, True)
+
+    if new_invariant < 0:
+        return
+    new_invariant_sol = gyro_two_math_testing.liquidityInvariantUpdate(scale(balances), scale(
+        sqrt_alpha), scale(sqrt_beta), scale(last_invariant), scale(diff_y), True)
+
+    assert to_decimal(new_invariant_sol) == scale(
+        new_invariant).approxed()
