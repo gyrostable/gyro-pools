38a68d91a09c8fc1538071575e9d957d077fb6f4
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2022-03-11 23:36:14 +0100

    tweak test performance

--- tests/cpmmv3/util.py
+++ tests/cpmmv3/util.py
@@ -12,10 +12,12 @@ from tests.support.utils import qdecimals
 
 def calculateInvariantUnderOver(gyro_three_math_testing, balances, root3Alpha):
     """Use like gyro_three_math_testing; we assert that we actually get an underestimate. No scaling is done."""
-    # Calling as transaction to get more debug info.
+    # Calling as transaction to get more debug info, and for gas estimation.
     # For example: run `brownie test` with `-I` and look at `history[-1].call_trace()`.
-    tx = gyro_three_math_testing.calculateInvariantUnderOver.transact(balances, root3Alpha)
-    l_under, under_is_under, l_over = tx.return_value
+    # Default off for speed (not sure how much of a difference this actually makes)
+    # tx = gyro_three_math_testing.calculateInvariantUnderOver.transact(balances, root3Alpha)
+    # l_under, under_is_under, l_over = tx.return_value
+    l_under, under_is_under, l_over = gyro_three_math_testing.calculateInvariantUnderOver(balances, root3Alpha)
     assert under_is_under
     return l_under, l_over
 
