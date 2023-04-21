3044331093f64fc1310f434906bc78af0676713e
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2023-04-21 13:38:44 +0200

    Supply computation: Various fixes, ECLP rate scaling

--- misc/compute_supply.py
+++ misc/compute_supply.py
@@ -7,21 +7,30 @@ from os import path
 sys.path.insert(0, path.dirname(path.dirname(__file__)))
 
 from tests.support.quantized_decimal import QuantizedDecimal
-from tests.support.types import ECLPMathParamsQD
+from tests.support.types import ECLPMathParamsQD, convd, params2MathParams, paramsTo100
 from tests.geclp import eclp_prec_implementation
 
+from tests.geclp import eclp_100 as mimpl
+from tests.support.quantized_decimal_100 import QuantizedDecimal as D3
 
 from scripts.coingecko import get_prices
 from scripts.constants import DECIMALS, TOKEN_ADDRESSES
 from scripts.pool_utils import compute_bounds_sqrts
 
+from brownie import chain
+
 
 TWO_CLP_L_INIT = Decimal("1e-2")  # can set to w/e, choose so that x,y are small
 THREE_CLP_L_INIT = 100  # can set to w/e, choose so that x,y,z are small
+E_CLP_L_INIT = 100  # can set to w/e, choose so that x,y,z are small
+# SOMEDAY ^ The value of these depends on the parameters actually. A more stable variant would be to
+# initialize from portfolio value instead.
 
-parser = argparse.ArgumentParser(prog="compute-initial-supply")
+parser = argparse.ArgumentParser(
+    prog="compute-initial-supply",
+    description="Compute initial supply. Run via `brownie run --network=<your network>`",
+    )
 parser.add_argument("config")
-parser.add_argument("-c", "--chain-id", type=int, default=137)
 parser.add_argument("-o", "--output")
 
 
@@ -31,20 +40,22 @@ def compute_amounts_2clp(pool_config: dict, chain_id: int):
     token_addresses = [TOKEN_ADDRESSES[chain_id][t] for t in pool_config["tokens"]]
     dx, dy = [DECIMALS[t] for t in pool_config["tokens"]]
     sqrt_alpha, sqrt_beta = compute_bounds_sqrts(token_addresses, pool_config["bounds"])
-    if token_addresses[0] >= token_addresses[1]:
-        token_addresses = token_addresses[::-1]
-        dx, dy = dy, dx
+    assert token_addresses[0] <= token_addresses[1]
 
     prices = get_prices(token_addresses, chain_id)
     px, py = [Decimal.from_float(prices[a]) for a in token_addresses]
+    pr = px / py
+
+    assert pr >= sqrt_alpha**2
+    assert pr <= sqrt_beta**2
 
     L_init = TWO_CLP_L_INIT
 
-    pr = px / py
     x = L_init * (1 / pr.sqrt() - 1 / sqrt_beta)
     y = L_init * (pr.sqrt() - sqrt_alpha)
-    S_init = L_init * 2
+    S_init = x * pr + y
     p_bpt = (x * px + y * py) / S_init
+    # ^ p_bpt = py up to rounding errors by design
 
     return {
         "amounts": {
@@ -64,30 +75,34 @@ def compute_amounts_2clp(pool_config: dict, chain_id: int):
 def compute_amounts_3clp(pool_config: dict, chain_id: int):
     assert len(pool_config["tokens"]) == 3, "ECLP should have 3 tokens"
 
-    tokens = sorted(
-        [(TOKEN_ADDRESSES[chain_id][t], DECIMALS[t]) for t in pool_config["tokens"]],
+    tokens = [(TOKEN_ADDRESSES[chain_id][t], DECIMALS[t]) for t in pool_config["tokens"]]
+    tokens_sorted = sorted(
+        tokens,
         key=lambda x: x[0].lower(),
     )
+    assert tokens == tokens_sorted
+
     prices_dict = get_prices([t for t, _ in tokens], chain_id)
     prices = [Decimal.from_float(prices_dict[t]) for t, _ in tokens]
+    px, py, pz = prices
+    pxz, pyz = px/pz, py/pz
 
-    px = prices[1] / prices[0]
-    py = prices[2] / prices[0]
     cbrt_alpha = Decimal(pool_config["root_3_alpha"])
 
-    assert px * py >= cbrt_alpha**3
-    assert px / py**2 >= cbrt_alpha**3
-    assert py / px**2 >= cbrt_alpha**3
+    assert pxz * pyz >= cbrt_alpha**3
+    assert pxz / pyz**2 >= cbrt_alpha**3
+    assert pyz / pxz**2 >= cbrt_alpha**3
 
     L_init = THREE_CLP_L_INIT
 
-    cbrtpxpy = (px * py) ** (Decimal("1") / 3)
-    x = L_init * (cbrtpxpy / px - cbrt_alpha)
-    y = L_init * (cbrtpxpy / py - cbrt_alpha)
+    cbrtpxpy = (pxz * pyz) ** (Decimal("1") / 3)
+    x = L_init * (cbrtpxpy / pxz - cbrt_alpha)
+    y = L_init * (cbrtpxpy / pyz - cbrt_alpha)
     z = L_init * (cbrtpxpy - cbrt_alpha)
     amounts = [x, y, z]
-    S_init = L_init * 3
-    p_bpt = (px * x + y + z) / S_init
+    S_init = x * pxz + y * pyz + z
+    p_bpt = (x * px + y * py + z * pz) / S_init
+    # ^ p_bpt = pz up to rounding errors by design.
 
     return {
         "amounts": {t: round(v * 10**d) for (t, d), v in zip(tokens, amounts)},
@@ -97,29 +112,64 @@ def compute_amounts_3clp(pool_config: dict, chain_id: int):
         "price_bpt": float(p_bpt),
     }
 
+import sys
+
+def info(type, value, tb):
+    if hasattr(sys, 'ps1') or not sys.stderr.isatty() or type != AssertionError:
+        # we are in interactive mode or we don't have a tty-like
+        # device, so we call the default hook
+        sys.__excepthook__(type, value, tb)
+    else:
+        import traceback, pdb
+        # we are NOT in interactive mode, print the exception...
+        traceback.print_exception(type, value, tb)
+        print
+        # ...then start the debugger in post-mortem mode.
+        pdb.pm()
+
+sys.excepthook = info
 
 def compute_amounts_eclp(pool_config: dict, chain_id: int):
     assert len(pool_config["tokens"]) == 2, "ECLP should have 2 tokens"
 
     token_addresses = [TOKEN_ADDRESSES[chain_id][t] for t in pool_config["tokens"]]
     dx, dy = [DECIMALS[t] for t in pool_config["tokens"]]
-    if token_addresses[0] >= token_addresses[1]:
-        token_addresses = token_addresses[::-1]
-        dx, dy = dy, dx
+    assert token_addresses[0] <= token_addresses[1]
 
     prices = get_prices(token_addresses, chain_id)
     px, py = [Decimal.from_float(prices[a]) for a in token_addresses]
 
+    # Rate scaling
+    rate_providers_dict = config.get("rate_providers", dict())
+    rate_provider_addresses = [rate_providers_dict.get(k) for k in pool_config["tokens"]]
+    rx, ry = get_rates(rate_provider_addresses)
+
+    # rate-scaled relative price
+    pr_s = ry / rx * px / py
+
     params = ECLPMathParamsQD(
         **{k: QuantizedDecimal(v) for k, v in pool_config["params"].items()}
     )
-    derived_params = eclp_prec_implementation.calc_derived_values(params)
+    # derived_params = eclp_prec_implementation.calc_derived_values(params)
 
-    # TODO: add computation
-    x = 0
-    y = 0
-    S_init = 0
-    p_bpt = 0
+    assert pr >= params.alpha
+    assert pr <= params.beta
+
+    # We run ECLP math calculations using the "old" (unoptimized) implementation but in 100
+    # decimals b/c we don't have an optimized implementation for this.
+    pr_100 = convd(pr_s, D3)
+    r_100 = convd(E_CLP_L_INIT, D3)
+    mparams_100 = params2MathParams(paramsTo100(params))
+    eclp = mimpl.ECLP.from_px_r(pr_100, r_100, mparams_100)
+    x_s, y_s = convd(eclp.x, QuantizedDecimal), convd(eclp.y, QuantizedDecimal)
+
+    # Go from rate-scaled to non-rate-scaled amounts
+    x = x_s / rx
+    y = y_s / ry
+
+    S_init = x_s * pr_s + y_s
+    p_bpt = (x * px + y * py) / S_init
+    # ^ p_bpt = py / ry up to rounding errors by design.
 
     return {
         "amounts": {
@@ -138,15 +188,16 @@ def compute_amounts_eclp(pool_config: dict, chain_id: int):
 
 def main():
     args = parser.parse_args()
+    chain_id = chain.id
     with open(args.config) as f:
         pool_config = json.load(f)
         pool_type = pool_config["pool_type"]
         if pool_type == "eclp":
-            result = compute_amounts_eclp(pool_config, args.chain_id)
+            result = compute_amounts_eclp(pool_config, chain_id)
         elif pool_type == "2clp":
-            result = compute_amounts_2clp(pool_config, args.chain_id)
+            result = compute_amounts_2clp(pool_config, chain_id)
         elif pool_type == "3clp":
-            result = compute_amounts_3clp(pool_config, args.chain_id)
+            result = compute_amounts_3clp(pool_config, chain_id)
         else:
             raise ValueError(f"invalid pool type {pool_type}")
     if args.output:
