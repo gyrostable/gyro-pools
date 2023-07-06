dd690c007b75da3a3986d0f8b062d0c60554ddbe
Daniel Perez <daniel@perez.sh>
2023-07-06 18:47:52 +0100

    Change Coingecko API endpoint

--- scripts/coingecko.py
+++ scripts/coingecko.py
@@ -1,7 +1,8 @@
-from typing import Dict, List
+from typing import Dict, List, Callable, TypeVar
 
 import requests
 
+T = TypeVar("T")
 
 LEGACY_MAPPINGS = {
     "0x9c9e5fd8bbc25984b178fdce6117defa39d2db39": "0xdab529f40e671a1d4bf91361c21bf9f0c9712ab7",
@@ -14,21 +15,50 @@ def get_asset_platforms():
     return r.json()
 
 
+def get_coins() -> List[dict]:
+    r = requests.get(
+        "https://api.coingecko.com/api/v3/coins/list?include_platform=true"
+    )
+    r.raise_for_status()
+    return r.json()
+
+
+def find(
+    haystack: List[T],
+    predicate: Callable[[T], bool],
+    error_msg: str = "could not find item in list",
+) -> T:
+    for item in haystack:
+        if predicate(item):
+            return item
+    raise ValueError(error_msg)
+
+
 def get_asset_platform_id(chain_id: int) -> str:
     asset_platforms = get_asset_platforms()
-    for asset_platform in asset_platforms:
-        if asset_platform["chain_identifier"] == chain_id:
-            return asset_platform["id"]
-    raise ValueError(f"could not find asset platform for chain id {chain_id}")
+    return find(asset_platforms, lambda x: x["chain_identifier"] == chain_id)["id"]
+
+
+def get_coin_ids(addresses: List[str], platform_id: str) -> List[str]:
+    coins = get_coins()
+    coin_ids = []
+    for address in addresses:
+        coin = find(
+            coins,
+            lambda x: x["platforms"].get(platform_id, "").lower() == address.lower(),
+        )
+        coin_ids.append(coin["id"])
+    return coin_ids
 
 
 def get_prices(addresses: List[str], chain_id: int = 1) -> Dict[str, float]:
     asset_platform_id = get_asset_platform_id(chain_id)
     mapped_addresses = [LEGACY_MAPPINGS.get(a.lower(), a) for a in addresses]
-    contract_addresses = ",".join(mapped_addresses)
-    base_url = "https://api.coingecko.com/api/v3/simple/token_price"
-    url = f"{base_url}/{asset_platform_id}?contract_addresses={contract_addresses}&vs_currencies=usd"
+    coin_ids = get_coin_ids(mapped_addresses, asset_platform_id)
+    formatted_ids = ",".join(coin_ids)
+    base_url = "https://api.coingecko.com/api/v3/simple/price"
+    url = f"{base_url}?ids={formatted_ids}&vs_currencies=usd"
     r = requests.get(url)
     r.raise_for_status()
     results = r.json()
-    return {a: results[ma.lower()]["usd"] for a, ma in zip(addresses, mapped_addresses)}
+    return {a: results[cid]["usd"] for a, cid in zip(addresses, coin_ids)}
